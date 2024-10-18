e64d21551832ade554b6890b5d96260ad7cc8554
RODRIGO BERGENTHAL <116042656+RodrigoBergenthal@users.noreply.github.com>
2024-10-18 19:01:09 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 
 import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 
 function App() {
 
   return (
-    <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
-      </main>
-    </>
-  );
+    <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects/>
+        <Contact />
+    </div>
+  )
 }
 
 export default App
